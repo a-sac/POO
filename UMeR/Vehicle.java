13de3f7a70b02950b4bd53866d899d3a320152e7
Tiago Alves <Tiago@MacBook-Air-de-Tiago.local>
2017-04-19 18:52:33 +0100

    Changed some variables' names and added more get methods.

--- UMeR/Vehicle.java
+++ UMeR/Vehicle.java
@@ -1,39 +1,59 @@
-
+import java.util.Queue;
 /**
  * Escreva a descrição da classe Vehicle aqui.
  *
  * @author tdaa
- * @version 1.0
+ * @version 19/04
  */
 public class Vehicle{
     private double avgSpeed;
-    private double price;
-    private int factor;
+    private double basePrice;
+    private double factor;
     private String plate;
+    private int type;
+    //to do queue e elementos dentro desta:
 
-    public Vehicle(double speed, double price, int factor, String plate){
+    public Vehicle(double speed, double price, double factor, String plate, int type){
       this.avgSpeed = speed;
-      this.price = price;
+      this.basePrice = price;
       this.factor = factor;
       this.plate = plate;
+      this.type = type;
     }
 
     public Vehicle(Vehicle v){
       this.avgSpeed = v.avgSpeed;
-      this.price = v.price;
+      this.basePrice = v.basePrice;
       this.factor = v.factor;
       this.plate = v.plate;
+      this.type = v.type;
+    }
+
+    public boolean isCar(){
+      return (this.type == 0);
+    }
+
+    public boolean isVan(){
+      return (this.type == 1);
+    }
+
+    public boolean isMotorbike(){
+      return (this.type == 2);
+    }
+
+    public int getType(){
+      return this.type;
     }
 
     public double getSpeed(){
       return this.avgSpeed;
     }
 
-    public double getPrice(){
-      return this.price;
+    public double getBasePrice(){
+      return this.basePrice;
     }
 
-    public int getFactor(){
+    public double getFactor(){
       return this.factor;
     }
 
