3bea68baf54fd924feeeb2e88289b9edb4ad599f
Tiago Alves <Tiago@MacBook-Air-de-Tiago.local>
2017-04-20 16:50:00 +0100

    Added the taxi class

---
+++ UMeR/Taxi.java
@@ -0,0 +1,39 @@
+import java.util.Queue;
+/**
+ * Escreva a descrição da classe Taxi aqui.
+ *
+ * @author tdaa
+ * @version 20/04
+ */
+public class Taxi{
+  private Driver driver;
+  private Client client;
+  private Point2D location;
+  private Queue<Client> waitingQ;
+
+  public Taxi(Taxi t){
+    this.driver = t.driver;
+    this.client = t.client;
+    this.location = t.location;
+    this.waitingQ = t.waitingQ;
+  }
+
+  public Taxi(Driver d, Client c, Point2D p, Queue<Client> q){
+    this.driver = d;
+    this.client = c;
+    this.location = p;
+    this.waitingQ = q;
+  }
+
+  public Driver getDriver(){
+    return this.driver;
+  }
+
+  public Client getClient(){
+    return this.client;
+  }
+
+  public Point2D getLocation(){
+    return this.location;
+  }
+}
